8f243f965e436a03180fbe6be3fce6306138f462
Max Rozen <3822106+rozenmd@users.noreply.github.com>
2023-02-28 22:15:11 +0100

    fix: make `onlineornot login` checked if you're logged in before doing anything

--- packages/onlineornot/src/login/index.ts
+++ packages/onlineornot/src/login/index.ts
@@ -1,6 +1,7 @@
 import { printBanner } from "../banner";
 import { logger } from "../logger";
 import openInBrowser from "../open-in-browser";
+import { getTokenQuietly } from "../user";
 
 export function loginOptions() {
 	return {};
@@ -8,8 +9,15 @@ export function loginOptions() {
 
 export async function loginHandler() {
 	const urlToOpen = "https://onlineornot.com/app/settings/developers";
+	const apiToken = getTokenQuietly();
 
 	await printBanner();
+
+	if (apiToken) {
+		return logger.log(
+			`You are already logged in with an API token.\nYou can generate another token at ${urlToOpen}`
+		);
+	}
 
 	logger.log(`Opening a link in your default browser: ${urlToOpen}`);
 	logger.log(
